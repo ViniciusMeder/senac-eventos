46457d5591547466fd557019ba8855933d11204c
Guilherme <guimazgt@gmail.com>
2026-05-11 01:02:27 -0300

    feat: Adicionado a segurança em TS e pagina basica do gestor (por Marcel) correção de bugs, estilização da pagina do gestor de acordo com o prototipo de baixa fideladade, responsividade para a pagina do gestor e funcionalidades da pagina do gestor(botões, abas navegaveis, barra lateral diferente para mobile e etc)

--- senac-eventos-clean/src/App.tsx
+++ senac-eventos-clean/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Gestor from "./pages/Gestor";
 
 // define as rotas da aplicacao
 export default function App() {
@@ -9,6 +10,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/gestor" element={<Gestor />} />
       </Routes>
     </BrowserRouter>
   );
